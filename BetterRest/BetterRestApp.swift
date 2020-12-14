fa2c6a04feba965e69ff1a564549e7eb85118032
The Road to Swift <theroadtoswift@gmail.com>
2020-12-14 16:45:23 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chris Direduryan on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
